944a03b58402e15264f3d671a4a708ac5fa850cc
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2025-12-03 22:49:44 +0800

    add day 3 solution

--- src/main/kotlin/day03/Main.kt
+++ src/main/kotlin/day03/Main.kt
@@ -14,7 +14,66 @@ fun main() {
 }
 
 fun part1(lines: List<String>) {
+    var sumOfJoltage = 0
+
+    for (line in lines) {
+        val numbers = line.map { it.toString().toInt() }
+
+        var head = 0
+        var tail = 0
+
+        var largestJoltage = 0
+        while (tail != numbers.size - 1) {
+            tail += 1
+            if ((numbers[tail] > numbers[head]) && tail != numbers.size - 1) {
+                head = tail
+                continue
+            }
+
+            val joltage = (numbers[head].toString() + numbers[tail].toString()).toInt()
+            if (joltage > largestJoltage) {
+                largestJoltage = joltage
+            }
+        }
+        sumOfJoltage += largestJoltage
+    }
+    println(sumOfJoltage)
 }
 
 fun part2(lines: List<String>) {
+    var sumOfJoltage = 0L
+
+    for (line in lines) {
+        val numbers = line.map { it.toString().toInt() }
+
+        var startIndex = numbers.size - 1 - 12
+        // search backwards for largest number
+        for (value in startIndex downTo 0) {
+            if (numbers[value] >= numbers[startIndex]) {
+                startIndex = value
+            }
+        }
+
+        val sequence = numbers.subList(startIndex, startIndex + 12).toMutableList()
+        var minIndex = indexOfMinInTheRight(sequence)
+        for (value in numbers.subList(startIndex + 12, numbers.size)) {
+            if (value >= sequence[minIndex]) {
+                sequence.removeAt(minIndex)
+                sequence.add(value)
+                minIndex = indexOfMinInTheRight(sequence)
+            }
+        }
+        sumOfJoltage += sequence.joinToString("").toLong()
+    }
+    println(sumOfJoltage)
+}
+
+fun indexOfMinInTheRight(numbers: List<Int>): Int {
+    var index = 0
+    for (i in 0..<numbers.size) {
+        if (numbers[i] < numbers[index]) {
+            index = i
+        }
+    }
+    return index
 }
