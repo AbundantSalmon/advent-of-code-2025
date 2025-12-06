1c27de397587c7d29233384f48a51d0951aae958
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2025-12-07 00:46:16 +0800

    add day 7 template

---
+++ src/main/kotlin/day07/Main.kt
@@ -0,0 +1,20 @@
+package day07
+
+import utils.FileUtils
+
+fun main() {
+    val filename = "input07.txt"
+    val lines = FileUtils.readFile(filename)
+
+    println("Part1:")
+    part1(lines)
+
+    println("Part2:")
+    part2(lines)
+}
+
+fun part1(lines: List<String>) {
+}
+
+fun part2(lines: List<String>) {
+}
