feaacde86928b9d725b38ddeb701eaf9afba9cda
AbundantSalmon <71184907+AbundantSalmon@users.noreply.github.com>
2025-12-12 23:14:03 +0800

    add day 7 part 1 solution

--- src/main/kotlin/day07/Main.kt
+++ src/main/kotlin/day07/Main.kt
@@ -7,14 +7,52 @@ fun main() {
     val lines = FileUtils.readFile(filename)
 
     println("Part1:")
-    part1(lines)
+    part1(lines.toMutableList())
 
     println("Part2:")
     part2(lines)
 }
 
-fun part1(lines: List<String>) {
+fun part1(lines: MutableList<String>) {
+    var numberOfSplits = 0
+    for (i in 1..<lines.size) {
+        val previousLine = lines[i - 1]
+        val currentLine = lines[i]
+
+        val mutableCurrentLine = currentLine.toCharArray().toMutableList()
+
+        for ((index, value) in mutableCurrentLine.withIndex()) {
+            if (previousLine[index] == 'S') {
+                mutableCurrentLine[index] = '|'
+            } else if (previousLine[index] == '|' && mutableCurrentLine[index] == '^') {
+                numberOfSplits++
+                mutableCurrentLine[index - 1] = '|'
+                mutableCurrentLine[index + 1] = '|'
+            } else if (previousLine[index] == '|') {
+                mutableCurrentLine[index] = '|'
+            }
+        }
+
+        lines[i] = mutableCurrentLine.joinToString("")
+    }
+
+    println(numberOfSplits)
 }
 
 fun part2(lines: List<String>) {
+    val splitterLocations: Set<Pair<Int, Int>> = mutableSetOf()
+    var startLocation: Pair<Int, Int>? = null
+    val maxY = lines.size - 1
+    for ((y, line) in lines.withIndex()) {
+        for ((x, value) in line.toCharArray().withIndex()) {
+            if (value == '^') {
+                splitterLocations.plus(Pair(x, y))
+            }
+            if (value == 'S') {
+                startLocation = Pair(x, y)
+            }
+        }
+    }
+
+    checkNotNull(startLocation)
 }
